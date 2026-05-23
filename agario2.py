d84a650ddb2bbcc63f16c142e954699ffd03738e
nivard <macuramatvej1@gmail.com>
2026-05-23 17:24:08 +0300

    Add files via upload

---
+++ agario2.py
@@ -0,0 +1,54 @@
+from math import hypot
+from pygame import *
+
+
+class Ball:
+    def __init__(self, speed, x, y, radius, color):
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+
+    def move(self):
+        keys = key.get_pressed()
+        if keys[K_w]:
+            self.y -= self.speed
+        if keys[K_s]:
+            self.y += self.speed
+        if keys[K_a]:
+            self.x -= self.speed
+        if keys[K_d]:
+            self.x += self.speed
+
+    def reset(self):
+        draw.circle(window, self.color, (self.x, self.y), self.radius)
+
+    def collidecircle(self, ball2):
+        distance = hypot(self.x - ball2.x, self.y - ball2.y)
+        return distance < (self.radius + ball2.radius)
+
+init()
+
+size = 500, 500
+
+window = display.set_mode(size)
+display.set_caption("Dota 4")
+clock = time.Clock()
+
+# bg = image.load("bg.img")
+# bg = transform.scale(bg, size)
+
+ball = Ball(300, 300, 25, 5, (255, 100, 255))
+
+while True:
+    for e in event.get():
+        if e.type == QUIT:
+            quit()
+
+    # window.blit(bg, (0, 0))
+    ball.move()
+    ball.reset()
+
+    display.update()
+    clock.tick(59)
